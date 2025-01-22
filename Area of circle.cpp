42e3b34fd1d21d845f1a3ab7d08c9377cf9f51cc
Sarthak Srivastava <ss6883@srmist.edu.in>
2025-01-22 15:30:50 +0530

    upd

--- Area of circle.cpp
+++ Area of circle.cpp
@@ -9,17 +9,37 @@ private:
     const float PI = 3.14159; // Private constant for the value of PI
 
 public:
-    Area()
-    {                                                      // Public constructor
-        cout << "Enter the radius of the circle: ";        // Prompt the user to enter the radius
-        cin >> radius;                                     // Read the radius from the user input
-        area = PI * radius * radius;                       // Calculate the area of the circle using the formula PI * r^2
-        cout << "Area of the circle is: " << area << endl; // Display the calculated area
+
+    int Input() // Public member function to read the radius from the user
+    {
+        cout << "Enter the radius of the circle: "; // Ask the user to enter the radius
+        cin >> radius;                              // Read the radius from the user
+
+        return 0;
     }
+
+    int findarea()
+    {                                                      // Public constructor                                   
+        area = PI * radius * radius;                       // Calculate the area of the circle using the formula PI * r^2
+
+        return 0;
+    }
+
+    int Output()
+    { // Public member function to display the area of the circle
+        cout << "The area of the circle is: " << area; // Display the area of the circle
+
+        return 0;
+    }
+
+
 };
 
 int main()
 {
     Area s; // Create an object of the Area class, which invokes the constructor
+    s.Input(); // Call the Input member function to read the radius from the user
+    s.findarea(); // Call the Area member function to calculate the area of the circle
+    s.Output(); // Call the Output member function to display the area of the circle
     return 0;
 }
