a3764a22a13bbd5c7633d40563252979493b7f19
Sarthak Srivastava <ss6883@srmist.edu.in>
2025-02-18 12:22:29 +0530

    Update Q10.cpp

--- Continuous Assessment/Q10.cpp
+++ Continuous Assessment/Q10.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+class HealthMonitor {
+private:
+    static int alertCount; // Declare alertCount
+public:
+    static int getAlertCount() {
+        return alertCount;
+    }
+};
+
+// Initialize alertCount outside the class definition
+int HealthMonitor::alertCount = 0;
+
+int main() {
+    cout << "Alert count: " << HealthMonitor::getAlertCount() << endl;
+    return 0;
+};
